a9d1ec41d8f5d38c3a4a52d5d230c86da0659888
Harcipan <92209277+Harcipan@users.noreply.github.com>
2023-10-18 17:32:28 +0200

    Delete ablak.h

--- ablak.h
+++
@@ -1,8 +0,0 @@
-#ifndef ABLAK_H_INCLUDED
-#define ABLAK_H_INCLUDED
-#include <SDL2/SDL.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
-void sdl_init(int szeles, int magas, SDL_Window **pwindow, SDL_Renderer **prenderer);
-Uint32 idozit(Uint32 ms, void *param);
-
-#endif // ABLAK_H_INCLUDED
