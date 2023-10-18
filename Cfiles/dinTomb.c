6bba88b1f6f381446be572bd8014d7fda4b39e1b
Harcipan <92209277+Harcipan@users.noreply.github.com>
2023-10-18 17:30:11 +0200

    AlapHF2
    folder management changes

---
+++ Cfiles/dinTomb.c
@@ -0,0 +1,110 @@
+#include "dinTomb.h"
+#include<stdbool.h>
+#include<stdlib.h>
+
+
+bool dintomb_atmeretez(DinTomb *kertTartalma, int ujmeretSor, int ujmeretOszlop)
+{
+    int **ujadat = (int **)malloc(ujmeretSor * sizeof(int *));
+    if (ujadat == NULL)
+        return false;
+
+    for (int i = 0; i < ujmeretSor; ++i)
+    {
+        ujadat[i] = (int *)malloc(ujmeretOszlop * sizeof(int));
+        if (ujadat[i] == NULL)
+        {
+            // Handle allocation failure and free previously allocated memory.
+            for (int j = 0; j < i; ++j)
+            {
+                free(ujadat[j]);
+            }
+            free(ujadat);
+            return false;
+        }
+    }
+
+    // Calculate the minimum dimensions to copy from the old data
+    int minSor = ujmeretSor < kertTartalma->meretSor ? ujmeretSor : kertTartalma->meretSor;
+    int minOszlop = ujmeretOszlop < kertTartalma->meretOszlop ? ujmeretOszlop : kertTartalma->meretOszlop;
+
+    // Copy data from the old array to the new array
+    for (int i = 0; i < minSor; ++i)
+    {
+        for (int j = 0; j < minOszlop; ++j)
+        {
+            ujadat[i][j] = kertTartalma->adat[i][j];
+        }
+    }
+
+    // Fill any remaining space in the new array with 0
+    for (int i = minSor; i < ujmeretSor; ++i)
+    {
+        for (int j = 0; j < ujmeretOszlop; ++j)
+        {
+            ujadat[i][j] = 0;
+        }
+    }
+
+    for (int j = minOszlop; j < ujmeretOszlop; ++j)
+    {
+        for (int i = 0; i < ujmeretSor; ++i)
+        {
+            ujadat[i][j] = 0;
+        }
+    }
+
+    // Free the old data if resizing is successful
+    for (int i = 0; i < kertTartalma->meretSor; ++i)
+    {
+        free(kertTartalma->adat[i]);
+    }
+    free(kertTartalma->adat);
+
+    // Update the structure with the new data and dimensions
+    kertTartalma->adat = ujadat;
+    kertTartalma->meretSor = ujmeretSor;
+    kertTartalma->meretOszlop = ujmeretOszlop;
+
+    return true;
+}
+
+
+bool dintomb_foglal(DinTomb *kertTartalma, int meretSor, int meretOszlop)
+{
+    kertTartalma->meretSor = meretSor;
+    kertTartalma->meretOszlop = meretOszlop;
+    kertTartalma->adat = (int**)malloc(meretSor * sizeof(int*));
+    if (kertTartalma->adat == NULL)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < meretSor; ++i)
+    {
+        kertTartalma->adat[i] = (int*)malloc(meretOszlop * sizeof(int));
+        if (kertTartalma->adat[i] == NULL)
+        {
+            // Handle allocation failure and free previously allocated memory.
+            for (int j = 0; j < i; ++j)
+            {
+                free(kertTartalma->adat[j]);
+            }
+            free(kertTartalma->adat);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void dintomb_feltolt(DinTomb *kertTartalma, int meretSor, int meretOszlop,int tolto)
+{
+    for (int i = 0; i < meretSor; ++i)
+    {
+        for (int j = 0; j < meretOszlop; ++j)
+        {
+            kertTartalma->adat[i][j] = tolto;
+        }
+    }
+}
---
+++ Cfiles/main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "ablak.h"
+#include "gameCycle.h"
+
+int main(int argc, char *argv[])
+{
+    //ablak meghívása
+    enum { ABLAKX=900, ABLAKY=400 };
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    sdl_init(ABLAKX, ABLAKY, &window, &renderer);
+    /* idozito hozzaadasa: 20 ms; 1000 ms / 24 ms -> 60 fps */
+    SDL_TimerID id = SDL_AddTimer(24, idozit, NULL);
+
+    //esemenyhurok
+    gameCycle(renderer);
+
+    // idozito torlese
+    SDL_RemoveTimer(id);
+    //kilépés
+    SDL_Quit();
+    return 0;
+}
