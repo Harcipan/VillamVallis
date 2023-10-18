76b3aff18111096b8a1d4fe020bbd389272c21fc
Harcipan <92209277+Harcipan@users.noreply.github.com>
2023-10-18 17:32:15 +0200

    Delete gameCycle.h

--- gameCycle.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAMECYCLE_H_INCLUDED
-#define GAMECYCLE_H_INCLUDED
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-void gameCycle(SDL_Renderer *renderer);
-#endif // GAMECYCLE_H_INCLUDED
